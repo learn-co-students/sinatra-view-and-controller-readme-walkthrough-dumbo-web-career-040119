d97887e3dd0d8036538c369ee745c6adfe99e28b
ftse105 <ftse105@gmail.com>
2019-04-24 10:12:54 -0400

    Done.

--- app.rb
+++ app.rb
@@ -6,11 +6,18 @@ class App < Sinatra::Base
   end
 
   post '/reverse' do
+    og = params["string"]
+    @og_reversed = og.reverse
+
     erb :reversed
   end
 
   get '/friends' do
     # Write your code here!
+    # @friends.each do |friend|
+    #   <h2><%= friend %</h2>
+    # end
 
+    erb :friends
   end
 end
